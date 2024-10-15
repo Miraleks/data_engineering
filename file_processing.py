e6efe02f6cbed6661b429439092b02d727e466e0
Alex Mironositskiy <drive_forever@mail.ru>
2024-10-15 23:18:29 +0200

    add create_csv_file, read_csv_file functions

--- file_processing.py
+++ file_processing.py
@@ -2,6 +2,7 @@ import yaml
 import json
 import os
 from functools import wraps
+import pandas as pd
 
 
 def ensure_directory_exists(func):
@@ -11,52 +12,71 @@ def ensure_directory_exists(func):
         if directory and not os.path.exists(directory):
             os.makedirs(directory)
         return func(file_path, *args, **kwargs)
+
     return wrapper
 
 
-dict_data_for_yaml = [{'countries': ['USA', 'Germany', 'France', 'Poland', 'Spain', 'Canada']},
-                      {'capital': ['Washington', 'Berlin', 'Paris', 'Warsaw', 'Madrid', 'Ottawa']}]
+dict_data_for_yaml = [
+    {"countries": ["USA", "Germany", "France", "Poland", "Spain", "Canada"]},
+    {"capital": ["Washington", "Berlin", "Paris", "Warsaw", "Madrid", "Ottawa"]},
+]
 
-dict_for_json = {
-    'name': 'John Smith',
-    'age': 26,
-    'city': 'New York',
-    'gender': 'male'
-}
+dict_for_json = {"name": "John Smith", "age": 26, "city": "New York", "gender": "male"}
+
+data_for_csv = [
+    {"user_name": "etl_user", "password": "123", "host": "127.0.0.1"},
+    {"user_name": "test_user", "password": "456", "host": "127.0.0.2"},
+]
 
 
 @ensure_directory_exists
 def create_yaml_file(file_path, data):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         documents = yaml.dump(data, file)
 
 
 @ensure_directory_exists
 def read_yaml_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         yaml_object = yaml.safe_load(file)
     return yaml_object
 
 
 @ensure_directory_exists
 def create_json_file(file_path, data):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         json.dump(data, file)
 
 
 @ensure_directory_exists
 def read_json_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         json_object = json.load(file)
     return json_object
 
 
+@ensure_directory_exists
+def create_csv_file(file_path, data):
+    df1 = pd.DataFrame(data)
+    df1.to_csv(file_path)
+
+
+@ensure_directory_exists
+def read_csv_file(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+
 if __name__ == "__main__":
-    yaml_file_path = r'.\data\store_file.yaml'
-    json_file_path = r'.\data\store_file.json'
+    yaml_file_path = r".\data\store_file.yaml"
+    json_file_path = r".\data\store_file.json"
+    csv_file_path = r".\data\store_file.csv"
 
     # create_yaml_file(file_path=yaml_file_path, data=dict_data_for_yaml)
     # print(read_yaml_file(yaml_file_path))
 
-    create_json_file(json_file_path, dict_for_json)
-    print(read_json_file(json_file_path))
+    # create_json_file(json_file_path, dict_for_json)
+    # print(read_json_file(json_file_path))
+
+    create_csv_file(csv_file_path, data_for_csv)
+    print(read_csv_file(csv_file_path))
